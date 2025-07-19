7f7a6b635b3037dee465bd9f3597b5922d27cd6b
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-19 22:30:34 +0300

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_EXPIRY } = require("../config/env");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Registration failed", error });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error });
+  }
+};
