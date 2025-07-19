5370ac2ffa3dc3164d5a74cba413498dba00b870
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-19 22:26:29 +0300

    Create env.js

---
+++ config/env.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "1d",
+  SMS_API_KEY: process.env.SMS_API_KEY
+};
