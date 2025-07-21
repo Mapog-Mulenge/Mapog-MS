49baadd8c100346c9cecd23df2037c5274072a68
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-21 06:20:07 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    console.log("🌐 Connecting to MongoDB Atlas...");
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // Timeout after 5s
     });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error("❌ MongoDB Connection Error", err.message);
-    process.exit(1);
+    console.log("✅ MongoDB Connected Successfully!");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1); // Exit process with failure
   }
 };
 
