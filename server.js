d94bda8b553e4cab79c51131bb806f8e07073ff0
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-21 03:11:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-// server.js
-const app = require("./app");
-const connectDB = require("./config/db");
+const app = require('./app');
+const PORT = process.env.PORT || 5000;
 
-// Load environment variables
-require("dotenv").config();
+app.get('/health', (req, res) => {
+  res.status(200).send('Backend is healthy ✅');
+});
 
-const PORT = process.env.PORT || 8080;
-
-// Connect to MongoDB
-connectDB();
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} 🚀`);
 });
