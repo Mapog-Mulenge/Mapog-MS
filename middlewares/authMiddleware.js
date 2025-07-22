6b26b7086c9c986737c7d8535397f7c9da9a12c7
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-22 15:27:50 +0300

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,21 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config/env");
 
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
-  const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
+    console.error(err);
     res.status(401).json({ message: "Invalid token" });
   }
 };
+
+module.exports = authMiddleware;
