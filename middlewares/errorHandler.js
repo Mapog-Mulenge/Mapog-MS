59168a64de65bde4678c42622fc85ea746dddf18
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-22 15:29:11 +0300

    Update errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,6 @@
-module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something went wrong", error: err.message });
+const errorHandler = (err, req, res, next) => {
+  console.error("Error:", err.stack);
+  res.status(500).json({ message: "Something went wrong" });
 };
+
+module.exports = errorHandler;
