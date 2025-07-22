b6307d7d96c09e3d0a9b712824f26d0fbf83a6eb
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-22 15:33:06 +0300

    Update schoolRoutes.js

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -4,5 +4,6 @@ const schoolController = require("../controllers/schoolController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", authMiddleware, schoolController.getAllSchools);
+router.post("/", authMiddleware, schoolController.createSchool);
 
 module.exports = router;
