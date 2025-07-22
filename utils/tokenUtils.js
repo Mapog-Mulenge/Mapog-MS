89ff1cde625ec982a6a7cec79f14cf686544d616
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-22 15:34:58 +0300

    Update tokenUtils.js

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET, JWT_EXPIRY } = require("../config/env");
+const { JWT_SECRET } = require("../config/env");
 
 exports.generateToken = (payload) => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRY });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
 };
