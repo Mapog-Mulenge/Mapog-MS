ca30cbe61143b625c17e534786d907b7f2b26229
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-22 15:27:08 +0300

    Update schoolController.js

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,10 +1,31 @@
 const School = require("../models/School");
 
+// Create new school
+exports.createSchool = async (req, res) => {
+  try {
+    const { name, location, type } = req.body;
+
+    const school = new School({
+      name,
+      location,
+      type,
+    });
+
+    await school.save();
+    res.status(201).json(school);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to create school" });
+  }
+};
+
+// Get all schools
 exports.getAllSchools = async (req, res) => {
   try {
     const schools = await School.find();
-    res.json(schools);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching schools", error });
+    res.status(200).json(schools);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to fetch schools" });
   }
 };
