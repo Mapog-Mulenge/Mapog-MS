b73c8ce156305bd114d51d1d4f2e2b276ad41fac
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-24 01:00:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express')
+const express = require('express');
+const next = require('next');
 const app = require("./app");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+const dev = process.env.NODE_ENV !== 'production
+const app = next({ dev })
+
+const handle = app.getRequestHandler();
+
+app.process().then(() => {
+  const server = express ()
+
+server all('*', (req, res) => {
+  return handle(req, res);           
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
