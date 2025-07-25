e83fc4fe0cf2c31c3598346a8bab0f02eee15ff1
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-25 14:08:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use("/api/pages", pageRoutes);
 //test route
 app.get('/ping', (req, res) =>
   res.send('pong'));
-);
 
 // Global Error Handler
 app.use(require("./middlewares/errorHandler"));
